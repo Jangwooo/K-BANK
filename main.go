8b0a9dd7902888846e9c72a8c76eb69ce714d362
jangwoo kim <kjw2262@gmail.com>
2021-10-14 20:43:31 +0900

    [ADD] API 추가

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"log"
+
 	"K-BANK/controller"
+	"K-BANK/middleware"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
 	r := gin.Default()
 
 	api := r.Group("/api")
@@ -13,6 +21,13 @@ func main() {
 		userAPI := api.Group("/user")
 		{
 			userAPI.POST("/signUp", controller.SignUpHandler)
+			userAPI.GET("/identity", controller.Identity)
+			userAPI.POST("/login", controller.LoginHandler)
+		}
+
+		bankAPI := api.Group("/banking").Use(middleware.Auth)
+		{
+			bankAPI.POST("/account", controller.OpenAccountHandler)
 		}
 	}
 	r.Static("images", "./images/")
