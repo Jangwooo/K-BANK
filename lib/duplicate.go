01f9060ee844cfd13ec61eba6ec31fa0e0f8fe22
jangwoo kim <kjw2262@gmail.com>
2021-10-14 20:40:54 +0900

    [EDIT] 중복 체크 로직 변경

--- lib/duplicate.go
+++ lib/duplicate.go
@@ -17,13 +17,9 @@ func DuplicateCheck(table string, column string, value string) bool {
 
 	fmt.Println(table, column, value)
 
-	query := fmt.Sprintf("select exists(select * from %s where %s = ?)", table, column)
+	query := fmt.Sprintf("SELECT EXISTS(select %s from %s where %s = ?)", column, table, column)
 
-	err = db.QueryRow(query, value).Scan(&result)
-	if err != nil {
-		log.Panic(err)
-	}
-	fmt.Println(result)
+	_ = db.Get(&result, query, value)
 
 	// 0: 중복 없음, 1: 중복 있음
 	if result == "1" {
