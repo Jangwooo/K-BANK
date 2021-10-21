45bd71c44efcd03c0cfd326d29ce1df3e10a2f46
jangwoo kim <kjw2262@gmail.com>
2021-10-21 20:03:46 +0900

    [EDIT] 로그인 로직 변경
    데이터 바인딩 로직 변경
    SQL -> GORM 적용

--- controller/login.go
+++ controller/login.go
@@ -2,8 +2,6 @@ package controller
 
 import (
 	"context"
-	"encoding/json"
-	"io/ioutil"
 	"net/http"
 	"time"
 
@@ -14,51 +12,31 @@ import (
 )
 
 type LoginRequest struct {
-	ID  string `json:"id"`
-	Pwd string `json:"pwd"`
-}
-
-func UnmarshalLoginRequest(data []byte) (LoginRequest, error) {
-	var r LoginRequest
-	err := json.Unmarshal(data, &r)
-	return r, err
+	ID  string `json:"id" binding:"required"`
+	Pwd string `json:"pwd" binding:"required"`
 }
 
 func LoginHandler(c *gin.Context) {
-	body, err := ioutil.ReadAll(c.Request.Body)
+	req := new(LoginRequest)
+	err := c.Bind(req)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"msg": err.Error(),
+		c.JSON(http.StatusBadRequest, gin.H{
+			"msg": "리퀘스트 형식이 잘못되었습니다",
 		})
 		return
 	}
 
-	req, err := UnmarshalLoginRequest(body)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"msg": err.Error(),
-		})
-		return
-	}
+	var u model.User
 
-	db, err := model.ConnectDB()
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"msg": err.Error(),
-		})
-		return
-	}
-	defer db.Close()
-
-	var pwd string
-	err = db.Get(&pwd, "SELECT password FROM user where user_id = ?", req.ID)
-	if err != nil {
+	r := model.DB.Where("id = ?", req.ID).Find(&u).RowsAffected
+	if r == 0 {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"msg": "일치하는 유저가 없습니다",
 		})
 		return
 	}
-	match := bcrypt.CompareHashAndPassword([]byte(pwd), []byte(req.Pwd))
+
+	match := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(req.Pwd))
 	if match != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"msg": "비밀번호가 일치하지 않습니다",
@@ -69,38 +47,8 @@ func LoginHandler(c *gin.Context) {
 	accessToken := lib.CreateToken()
 	refreshToken := lib.CreateToken()
 
-	r, err := model.ConnectRedis(model.AccessTokenDB)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"여기냐?": "그렇다!",
-			"msg":  err.Error(),
-		})
-		return
-	}
-	r2, err := model.ConnectRedis(model.RefreshTokenDB)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"여기냐?": "그렇다!",
-			"msg":  err.Error(),
-		})
-		return
-	}
-	r3, err := model.ConnectRedis(model.MobileTokenDB)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"여기냐?": "그렇다!",
-			"msg":  err.Error(),
-		})
-		return
-	}
-
-	if client := c.Request.Header.Get("client"); client == "mobile" {
-		r3.Set(context.Background(), accessToken, req.ID, 0)
-		refreshToken = ""
-	} else if client == "web" {
-		r.Set(context.Background(), accessToken, req.ID, time.Minute*2)
-		r2.Set(context.Background(), lib.CreateToken(), req.ID, time.Hour*24*7)
-	}
+	model.AccessTokenRedis.Set(context.Background(), accessToken, req.ID, time.Hour*2)
+	model.RefreshTokenRedis.Set(context.Background(), refreshToken, req.ID, time.Hour*24*7)
 
 	c.JSON(200, gin.H{
 		"refresh_token": refreshToken,
