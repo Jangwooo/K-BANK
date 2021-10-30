58b8ba283106cf6ea5f326ba8dc4a142428a06c6
jangwoo kim <kjw2262@gmail.com>
2021-10-31 03:43:42 +0900

    [FIX] 로직 오류

--- lib/duplicate.go
+++ lib/duplicate.go
@@ -6,10 +6,11 @@ import (
 
 // DuplicateCheck : 주어진 칼럼, 값을 가지고 그 값이 존재하는지 확인하는 함수
 func DuplicateCheck(column string, value string) bool {
-	result := model.DB.Limit(1).Where(column+"= ?", value).Find(&model.User{}).RowsAffected
+	var count int64
+	model.DB.Limit(1).Where(column+"= ?", value).Find(&model.User{}).Count(&count)
 
 	// 0: 중복 없음, 1: 중복 있음
-	if result == 0 {
+	if count == 0 {
 		return true
 	}
 	return false
