fa878ce2a015790fff2ad2ec75b2153eb911609a
jangwoo kim <kjw2262@gmail.com>
2021-11-22 14:51:56 +0900

    [FEATURE] 다른 은행에서 계좌정보 요청할때

---
+++ controller/bank/open/get_accounts.go
@@ -0,0 +1,36 @@
+package open
+
+import (
+	"net/http"
+
+	"K-BANK/model"
+	"github.com/gin-gonic/gin"
+)
+
+// GetAccounts is 다른 은행에서 이 사람이 가지고 있는 모든 계좌를 조회할때 사용하는 것
+func GetAccounts(c *gin.Context) {
+	type response struct {
+		Msg      string                  `json:"msg,omitempty"`
+		Accounts []model.CheckingAccount `json:"accounts,omitempty"`
+	}
+	res := response{}
+
+	phoneNumber := c.Param("phone_number")
+
+	var count int64
+	subQuery := model.DB.Select("id").Where("phone_number = ?", phoneNumber).Table("users")
+	err := model.DB.Where("user_id = (?)", subQuery).Find(&res.Accounts).Count(&count).Error
+
+	if err != nil {
+		panic(err)
+	}
+
+	if count == 0 {
+		res.Msg = "계좌가 없습니다"
+		c.JSON(http.StatusOK, res)
+		return
+	}
+
+	c.JSON(http.StatusOK, res)
+
+}
