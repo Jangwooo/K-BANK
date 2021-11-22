3efc7f26ab9b7422a152544d4c2e3e9699a7c994
jangwoo kim <kjw2262@gmail.com>
2021-11-22 14:46:54 +0900

    [ADD] 에러 핸들링

--- lib/duplicate.go
+++ lib/duplicate.go
@@ -1,13 +1,20 @@
 package lib
 
 import (
+	"errors"
+
 	"K-BANK/model"
+	"gorm.io/gorm"
 )
 
 // DuplicateCheck : 주어진 칼럼, 값을 가지고 그 값이 존재하는지 확인하는 함수
 func DuplicateCheck(column string, value string) bool {
 	var count int64
-	model.DB.Limit(1).Where(column+"= ?", value).Find(&model.User{}).Count(&count)
+	err := model.DB.Limit(1).Where(column+" = ?", value).Find(&model.User{}).Count(&count).Error
+
+	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		panic(err)
+	}
 
 	// 0: 중복 없음, 1: 중복 있음
 	if count == 0 {
