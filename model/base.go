1cbc9140b3188ab64caabee3335e39b1aaf5afd5
jangwoo kim <kjw2262@gmail.com>
2021-12-01 20:30:20 +0900

    [ADD] base url, 은행코드 추가

---
+++ model/base.go
@@ -0,0 +1,14 @@
+package model
+
+const (
+	TossURL  = "http://10.80.162.103:8000/"
+	MeoguURL = "http://10.80.163.17:8000/"
+	KaKaoURL = "http://10.80.162.195:8000/"
+)
+
+const (
+	KBankCode = "110"
+	TossCode  = "666"
+	KakaoCode = "108"
+	MeoguCode = "719"
+)
